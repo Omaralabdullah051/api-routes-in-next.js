b39361f54d4d93bce8844491e0d3528047df6ae0
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-01 15:32:51 +0400

    handled POST request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const CommentsPage = () => {
   const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
 
   const fetchComments = async () => {
     const res = await fetch("/api/comments");
@@ -9,8 +10,26 @@ const CommentsPage = () => {
     setComments(data);
   };
 
+  const submitComment = async () => {
+    const res = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <>
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
+      <button onClick={submitComment}>Submit Comment</button>
       <button onClick={fetchComments}>Load comments</button>
       {comments.map((comment) => (
         <div key={comment.id}>
